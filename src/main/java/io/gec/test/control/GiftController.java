7e1f6c2e10c71a6dc63f68301ba3e97925374f6e
Kai.Suchomel <kai.suchomel@iotos.io>
2021-07-06 16:07:23 +0200

    changed send to Message

--- src/main/java/io/gec/test/control/GiftController.java
+++ src/main/java/io/gec/test/control/GiftController.java
@@ -4,6 +4,8 @@ import io.gec.test.entity.Gift;
 import io.quarkus.scheduler.Scheduled;
 import io.quarkus.scheduler.ScheduledExecution;
 import io.smallrye.reactive.messaging.annotations.Broadcast;
+import io.smallrye.reactive.messaging.kafka.OutgoingKafkaRecordMetadata;
+import java.util.UUID;
 import java.util.concurrent.atomic.AtomicInteger;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -11,6 +13,8 @@ import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import org.eclipse.microprofile.reactive.messaging.Channel;
 import org.eclipse.microprofile.reactive.messaging.Emitter;
+import org.eclipse.microprofile.reactive.messaging.Message;
+import org.eclipse.microprofile.reactive.messaging.Metadata;
 import org.eclipse.microprofile.reactive.messaging.OnOverflow;
 
 @ApplicationScoped
@@ -35,7 +39,9 @@ public class GiftController {
     @Transactional
     public void doTransactionalStuff(Gift aProducedGift) {
         System.err.println("GiftController.doTransactionalStuff: " + aProducedGift);
+        Message<String> m = Message.of(aProducedGift.toString(), Metadata.of(OutgoingKafkaRecordMetadata.<String>builder()
+                    .withKey(UUID.randomUUID().toString()).build()));
         em.persist(aProducedGift);
-        emitter.send(aProducedGift.toString());
+        emitter.send(m);
     }
 }
